0a5395ca8544690d8fa61d36e600490329757ef8
Hamilton Chapman <hamchapman@gmail.com>
2015-10-09 10:51:44 +0300

    Initial Commit

---
+++ TablePickerView/TablePickerView.h
@@ -0,0 +1,19 @@
+//
+//  TablePickerView.h
+//  TablePickerView
+//
+//  Created by Hamilton Chapman on 09/10/2015.
+//  Copyright © 2015 hc.gg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TablePickerView.
+FOUNDATION_EXPORT double TablePickerViewVersionNumber;
+
+//! Project version string for TablePickerView.
+FOUNDATION_EXPORT const unsigned char TablePickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TablePickerView/PublicHeader.h>
+
+
